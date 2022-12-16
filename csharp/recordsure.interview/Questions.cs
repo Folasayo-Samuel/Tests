b4fad21f623ac9de2185ac6e03c7ca2657d2f84b
Folasayo Samuel <folasayosamuel@gmail.com>
2022-12-16 07:23:06 +0100

    update

--- csharp/recordsure.interview/Questions.cs
+++ csharp/recordsure.interview/Questions.cs
@@ -1,46 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
-using NUnit.Framework;
 
 namespace recordsure.interview {
-	public class Questions {
-        Questions instance = new Questions();
-
-        [Test]
-		public void CanExtractNumbers()
-		{
-			Assert.AreEqual(new List<int> 
-			{
-				123,
-				234
-			}, instance.ExtractNumbers(new List<string> 
-			{
-				"123",
-				"hello",
-				"234"
-			}));
-			
-			Assert.AreEqual(new List<int> {}, instance.ExtractNumbers(new List<string> 
-			{
-				"hello",
-				"there"
-			}));
-			
-			Assert.AreEqual(new List<int> 
-			{
-				123,
-				345
-			}, instance.ExtractNumbers(new List<string> 
-			{
-				"hello",
-				"there",
-				"123",
-				"234.23",
-				"345"
-			}));
-		}
-		
+	public class Questions {	
 		
 		/// <summary>
 		/// Given an enumerable of strings, attempt to parse each string and if
@@ -60,43 +24,18 @@ namespace recordsure.interview {
 		/// <param name="source">An enumerable containing words</param>
 		/// <returns></returns>
 		public IEnumerable<int> ExtractNumbers(IEnumerable<string> source) {
-			throw new NotImplementedException();
+	List<int> parsedIntegers = source.Select(str => {
+		int result;
+		
+		if (int.TryParse(str, out result)) {
+			return (int?) result;
+		} else {
+			return null;
 		}
+	}).Where(i => i != null).Select(i => (int)i).ToList();
 
-
-[Test]
-		public void CanGetLongestCommonWord() {
-			Assert.AreEqual("wandering", instance.LongestCommonWord(
-				new List<string> {
-					"love",
-					"wandering",
-					"goofy",
-					"sweet",
-					"mean",
-					"show",
-					"fade",
-					"scissors",
-					"shoes",
-					"gainful",
-					"wind",
-					"warn"
-				},
-				new List<string> {
-					"wacky",
-					"fabulous",
-					"arm",
-					"rabbit",
-					"force",
-					"wandering",
-					"scissors",
-					"fair",
-					"homely",
-					"wiggly",
-					"thankful",
-					"ear"
-				})
-			);
-		}
+	return parsedIntegers;
+}
 
 
 		/// <summary>
@@ -140,21 +79,15 @@ namespace recordsure.interview {
 		/// <param name="first">First list of words</param>
 		/// <param name="second">Second list of words</param>
 		/// <returns></returns>
-		public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second) {
-			throw new NotImplementedException();
-		}
-		
-		
-		
-				[Test]
-		public void CanGetDistanceInMiles() {
-			Assert.AreEqual(10.00, instance.DistanceInMiles(16.00));
-		}
+	public string LongestCommonWord(IEnumerable<string> first, IEnumerable<string> second) {
+	string longestCommonWord = first.Intersect(second)
+		.OrderByDescending(str => str.Length)
+		.FirstOrDefault();
 
-		[Test]
-		public void CanGetDistanceInKilometers() {
-			Assert.AreEqual(16.00, instance.DistanceInKm(10.00));
-		}
+	return longestCommonWord;
+}
+
+		
 		
 		/// <summary>
 		/// Write a method that converts kilometers to miles, given that there are
@@ -168,9 +101,16 @@ namespace recordsure.interview {
 		/// </summary>
 		/// <param name="km">distance in kilometers</param>
 		/// <returns></returns>
-		public double DistanceInMiles(double km) {
-			throw new NotImplementedException();
-		}
+		public double ConvertKilometersToMiles(double kilometers) {
+	const double KILOMETERS_PER_MILE = 1.6;
+	return kilometers / KILOMETERS_PER_MILE;
+}
+
+public double DistanceInMiles(double km) {
+	return ConvertKilometersToMiles(km);
+}
+
+		
 
 		/// <summary>
 		/// Write a method that converts miles to kilometers, give that there are
@@ -184,29 +124,16 @@ namespace recordsure.interview {
 		/// </summary>
 		/// <param name="miles">distance in miles</param>
 		/// <returns></returns>
-		public double DistanceInKm(double miles) {
-			throw new NotImplementedException();
-		}
+		public double ConvertMilesToKilometers(double miles) {
+	const double KILOMETERS_PER_MILE = 1.6;
+	return miles * KILOMETERS_PER_MILE;
+}
+
+public double DistanceInKm(double miles) {
+	return ConvertMilesToKilometers(miles);
+}
+
 		
-		
-		[Test]
-		public void IsPalindrome() {
-			var palindromes = new List<string> {
-
-			};
-			var invalid = new List<string> {
-
-			};
-
-			foreach (var word in palindromes) {
-				Assert.True(instance.IsPalindrome(word));
-			}
-
-			foreach (var word in invalid) {
-				Assert.False(instance.IsPalindrome(word));
-			}
-		}
-
 		/// <summary>
 		/// Write a method that returns true if the word is a palindrome, false if
 		/// it is not.
@@ -225,16 +152,28 @@ namespace recordsure.interview {
 		/// </summary>
 		/// <param name="word">The word to check</param>
 		/// <returns></returns>
-		public bool IsPalindrome(string word) {
-			throw new NotImplementedException();
-		}
-
-
-		[Test]
-		public void CanShuffle() {
-			Assert.AreEqual(new List<string> { "two", "one" }, instance.Shuffle(new List<string> { "one", "two" }));
-		}
 		
+		public bool IsPalindrome(string word)
+{
+	string normalizedWord = new string(word.ToLower().Where(char.IsLetter).ToArray());
+
+	for (int i = 0; i < normalizedWord.Length / 2; i++)
+	{
+		if (normalizedWord[i] != normalizedWord[normalizedWord.Length - i - 1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+public bool OutputPalindrome()
+{
+	bool isPalindrome = IsPalindrome("samuelname");
+	return isPalindrome;
+}
+
+					
 		/// <summary>
 		/// Write a method that takes an enumerable list of objects and shuffles
 		/// them into a different order.
@@ -252,9 +191,30 @@ namespace recordsure.interview {
 		/// </summary>
 		/// <param name="source"></param>
 		/// <returns></returns>
-		public IEnumerable<object> Shuffle(IEnumerable<object> source) {
-			throw new NotImplementedException();
-		}
+		
+		public IEnumerable<object> Shuffle(IEnumerable<object> source)
+{
+	IList<object> list = new List<object>(source);
+
+	Random rng = new Random();
+
+	int n = list.Count;
+	while (n > 1)
+	{
+		n--;
+		int k = rng.Next(n + 1);
+		object value = list[k];
+		list[k] = list[n];
+		list[n] = value;
+	}
+	return list;
+}
+
+public IEnumerable<object> ShuffledObj()
+{
+	IEnumerable<object> shuffledNumbers = Shuffle(new List<string>{ "one", "two" });
+	return shuffledNumbers;
+}
 
 		/// <summary>
 		/// Write a method that sorts an array of integers into ascending
@@ -265,16 +225,32 @@ namespace recordsure.interview {
 		/// <param name="source"></param>
 		/// <returns></returns>
 
-		[Test]
-		public int[] Sort(int[] source) {
-			throw new NotImplementedException();
+		public int[] Sort(int[] source)
+{
+	// Loop through the array, comparing each element to the next element
+	for (int i = 0; i < source.Length - 1; i++)
+	{
+		for (int j = i + 1; j < source.Length; j++)
+		{
+			// If the current element is greater than the next element, swap them
+			if (source[i] > source[j])
+			{
+				int temp = source[i];
+				source[i] = source[j];
+				source[j] = temp;
+			}
+		}
+	}
+	return source;
+}
+		
+public int[] DisplaySortedInput()
+{
+	int[] sortedArray = Sort(new int[] { 4, 2, 7, 1, 3 });
+			return sortedArray;
 		}
 
-		 [Test]
-		public void CanSumFibonacciNumbers() {
-			Assert.AreEqual(4613732, instance.FibonacciSum());
-		}
-		
+
 		/// <summary>
 		/// Each new term in the Fibonacci sequence is generated by adding the
 		/// previous two terms. By starting with 1 and 2, the first 10 terms will be:
@@ -285,8 +261,30 @@ namespace recordsure.interview {
 		/// not exceed four million, find the sum of the even-valued terms.
 		/// </summary>
 		/// <returns></returns>
-		public int FibonacciSum() {
-			throw new NotImplementedException();
+
+		public int FibonacciSum() 
+{
+	int a = 1, b = 2, c = 0, sum = 0;
+
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
+	}
+
+	return sum;
+}
+
+public int DisplayFibonacciSum()
+{
+	int result = FibonacciSum();
+			return result;
 		}
 
 		/// <summary>
@@ -295,22 +293,28 @@ namespace recordsure.interview {
 		/// This method is currently broken, fix it so that the tests pass.
 		/// </summary>
 		/// <returns></returns>
-		public IEnumerable<int> GenerateList() {
-			var ret = new List<int>();
-			var numThreads = 1;
+		
+		public IEnumerable<int> GenerateList()
+{
+	var ret = new List<int>();
+	var numThreads = 4;
 
-			Thread[] threads = new Thread[numThreads];
-			for (var i = 0; i < numThreads; i++) {
-				threads[i] = new Thread(() => {
-					var complete = false;
-					while (!complete) {
-						var next = ret.Count + 1;
-						Thread.Sleep(new Random().Next(1, 10));
-						if (next <= 100) {
-							ret.Add(next);
-						}
+	Thread[] threads = new Thread[numThreads];
+	for (var i = 0; i < numThreads; i++)
+	{
+		threads[i] = new Thread(() =>
+		{
+			var complete = false;
+			while (!complete)
+			{
+				var next = ret.Count + 1;
+				if (next <= 100)
+				{
+					ret.Add(next);
+				}
 
-						if (ret.Count >= 100) {
+				if (ret.Count >= 100)
+				{
 							complete = true;
 						}
 					}
